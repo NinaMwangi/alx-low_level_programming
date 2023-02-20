d3e8c87c888bb4ad427f1029519fbcbd0800cd49
Nina Mwangi <122353603+NinaMwangi@users.noreply.github.com>
2023-02-20 18:52:12 +0300

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - main entry point
+*return - always zero meaning success
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
