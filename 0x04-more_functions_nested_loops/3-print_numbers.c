48b026816a8b039df6db19363529cb706021a9f7
Nina Mwangi <122353603+NinaMwangi@users.noreply.github.com>
2023-02-23 19:31:06 +0300

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * void print_numbers - prints numbers from 0 to 0
+ * Description: The for loop iterates through the sequence
+ * Return: 0 upto 9
+ */
+
+void print_numbers(void)
+{
+	int i;
+	for (i = 0; i <= 9; i++)
+	{
+	_putchar(i + '0');
+	}
+	_putchar('\n');
+}
