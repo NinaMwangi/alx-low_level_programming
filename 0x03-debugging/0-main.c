b88800aafa2ae074b986b165c30df8c29b8a4c7c
Nina Mwangi <122353603+NinaMwangi@users.noreply.github.com>
2023-02-24 00:29:08 +0300

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h'
+
+/**
+ * main - execution point
+ * Description: to test a function
+ * Return: 0 for now
+ */
+
+int main(void)
+{
+        int i;
+
+        i = 0
+        positive_or_negative(i);
+        return (0);
+}
