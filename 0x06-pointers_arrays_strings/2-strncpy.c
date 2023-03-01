38c7a34717c3992f6f5c851b8ce8230934b61553
Nina Mwangi <122353603+NinaMwangi@users.noreply.github.com>
2023-03-01 15:11:59 +0300

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @src: The source
+ * @dest: string destination
+ * @n: the maximum number of bytes to be copied from src
+ * Return: The pointer to the resulting string dest.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
+}
