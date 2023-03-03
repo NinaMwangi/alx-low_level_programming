3dacdbb3c4274ee42dfb873b50cc19373744e6f0
Nina Mwangi <122353603+NinaMwangi@users.noreply.github.com>
2023-03-03 04:35:13 +0300

    Task 103

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/*
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
 	int n;
@@ -9,12 +15,14 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 /*
- * main - Entry point
- * @a: the array being appended
+ * main - check the code
+ * Description: in the code
+ * Remember:
  * - you are not allowed to use a
  * - you are not allowed to modify p
  * - only one statement
  * - you are not allowed to code anything else than this line of code
+ * Return: Always 0.
  */
 	*(p + 5) = 98;
  /* ...so that this prints 98\n */
