6b4019013bc5351223509d93f4884ab6911ddf59
Nina Mwangi <122353603+NinaMwangi@users.noreply.github.com>
2023-03-10 22:03:17 +0300

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry function
+ * @argc: size of array
+ * @argv: Array of arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char __attribute__((__unused__)) **argv)
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
