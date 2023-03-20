c8b323310f7477be61b12073f2f0fe5562ca33b1
Nina Mwangi <122353603+NinaMwangi@users.noreply.github.com>
2023-03-20 19:17:53 +0300

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - New type describing dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
