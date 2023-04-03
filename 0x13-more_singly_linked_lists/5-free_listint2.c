01e71ab403bc1b23880c5f2affdd777afdf7d91b
Nina Mwangi <122353603+NinaMwangi@users.noreply.github.com>
2023-04-03 21:34:49 +0300

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees the list and sets the head to NULL.
+ * @head: The beginning of the list.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+
+	head = NULL;
+}
